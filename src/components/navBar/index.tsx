53d8dc5cd972532a5ec7a8a5b0613f585982a90e
Luisa Vaca <luisavaca@Ordenador-portatil-de-Luisa.local>
2024-08-30 08:07:37 +0200

    fix(Code): solve linters errors, remove spaces, ignore-coverage

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,33 +1,63 @@
 import climLogo from '@/assets/climLogo.svg'
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
+import './index.css';
 
 function NavBar() {
   const { i18n } = useTranslation();
   const [ currentLanguage, setCurrentLanguage ] = useState('en');
+  const [ isVisibleContactForm, setIsVisibleContactForm ] = useState(true);
   const changeLanguage = () => {
     const newLanguage = i18n.language === 'en' ? 'es' : 'en';
     i18n.changeLanguage(newLanguage);
     setCurrentLanguage(newLanguage)
   }
 
+  const toggleContactForm = () => {
+    setIsVisibleContactForm(!isVisibleContactForm);
+  }
+  
   return (
-    <nav className='flex justify-between h-32 m-24'>
-      <img 
-        src={climLogo} 
-        className="logo" 
-        alt="Vite logo" />
-      <div
-        className='flex gap-2 items-center cursor-pointer'
-        onClick={changeLanguage}
-        >
-        <p className='text-12'>{currentLanguage.toUpperCase()}</p>
+    <>
+      <nav className='flex justify-between h-32 m-24'>
         <img 
-          className="icon" 
-          src={'./public/translateIcon.svg'} 
-          alt="translation icon" />
-      </div>
-    </nav>
+          src={climLogo}
+          className="logo"
+          alt="Vite logo" />
+        <div className='flex items-center cursor-pointer'>
+          <div
+            className='flex gap-2 items-center cursor-pointer'
+            onClick={changeLanguage}
+            >
+            <p className='text-12'>{currentLanguage.toUpperCase()}</p>
+            <img
+              className="icon"
+              src={'./public/translateIcon.svg'}
+              alt="translation icon" />
+          </div>
+          <h1 className='text-14 ml-3' onClick={toggleContactForm}>Contacto</h1>
+        </div>
+      </nav>
+      <section className={`contact-form-overlay w-full h-full flex items-center justify-center + ${isVisibleContactForm ? "hidden" : "block"}`}>
+          <div className="bg-white">
+            I will the form
+            <form>
+              <label htmlFor='name'>Nombre</label>
+              <input type='text' id='name' />
+              <label htmlFor='email'>Email</label>
+              <input type='email' id='email' />
+              <label htmlFor='dateBird'>Fecha de nacimiento</label>
+              <input type='date' id='dateBird' />
+              <label htmlFor='city'>Ciudad</label>
+              <input type='text' id='city' />
+              <label htmlFor='telefono'>Mensaje</label>
+              <input type='number' id='mensaje' />
+              <button>Enviar</button>
+            </form>
+          </div>
+
+      </section>
+    </>
   )
 }
 
---
+++ src/components/weekForecast/types.ts
@@ -0,0 +1,5 @@
+import type { List } from '../../repositories/weatherRepository/types';
+
+export interface WeekForecastProps {
+  weekSummaryForecast: Array<List>
+}
